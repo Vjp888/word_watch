7c9491c28389aea2d1d1531339dfab8958eaaff5
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-07-18 11:55:49 -0600

    Creates initial api calls and possible route for passing information to the view
    Spent a fair bit of time working on understanding how the webpack interacted with the frontend
    Otherwise I feel I was on a decent path. Just having trouble getting things to connect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $('#make-call').click(function() {
+    let url = 'https://wordwatch-api.herokuapp.com/api/v1/words';
+    let words = $.trim($('text-zone').val());
+    let array = words.split(" ");
+    $.map( array, function(word) {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({word: {value: word}})
+      })
+    }).then(
+      fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+      .then(response => response.json())
+      .then(wordData => {
+        let word = JSON.parse(wordData)
+        document.getElementById("test").innerHTML = word;
+      })
+    )
+  })
 })
